24433c1496d5efe5b9cb7fe735cadef213c6e290
Luis Villarreal <luisdaniel_1996@hotmail.com>
2018-03-14 20:57:35 -0600

    Added constraints to map. Map now shows markers and paths (will be used for testing and updating data).

--- CampusAccesible/MapViewController.swift
+++ CampusAccesible/MapViewController.swift
@@ -26,7 +26,7 @@ class MapViewController: UIViewController {
         // Creación del mapa
         let camera = GMSCameraPosition.camera(withLatitude: 25.651130, longitude: -100.289599, zoom: 17.0)
         mapView.camera = camera
-        
+
         // Restricciones del movimiento del mapa
         mapView.setMinZoom(17.0, maxZoom: 21.0)
         let upperLeftBound = CLLocationCoordinate2D(latitude: 25.653485, longitude: -100.292080)
@@ -43,6 +43,30 @@ class MapViewController: UIViewController {
         keyPoints = NSArray(contentsOfFile: keyPointsPath!)
         let pathsPath = Bundle.main.path(forResource: "ListaCaminos", ofType: "plist")
         paths = NSArray(contentsOfFile: pathsPath!)
+        
+        for (index, element) in locations.enumerated() {
+            let marker = GMSMarker()
+            let location = element as! NSDictionary
+            marker.position = CLLocationCoordinate2D(latitude: location.value(forKey: "longitud") as! Double, longitude: location.value(forKey: "latitud") as! Double)
+            marker.title = String(marker.position.latitude) + " " + String(marker.position.longitude)
+            marker.snippet = String(index)
+            marker.map = mapView
+        }
+        
+        for element in paths {
+            let path = element as! NSDictionary
+            let pointPath = GMSMutablePath()
+            let point1 = path.value(forKey: "punto1") as! Int
+            let point2 = path.value(forKey: "punto2") as! Int
+            let dic1 = locations[point1] as! NSDictionary
+            let dic2 = locations[point2] as! NSDictionary
+            pointPath.add(CLLocationCoordinate2D(latitude: dic1.value(forKey: "longitud") as! Double, longitude: dic1.value(forKey: "latitud") as! Double))
+            pointPath.add(CLLocationCoordinate2D(latitude: dic2.value(forKey: "longitud") as! Double, longitude: dic2.value(forKey: "latitud") as! Double))
+            let line = GMSPolyline(path: pointPath)
+            line.map = mapView
+            line.strokeWidth = 5
+        }
+        
         
         // Estilo y datos que se filtran
         tfFrom.borderStyle = UITextBorderStyle.roundedRect
